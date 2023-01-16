b9ef56739b73b8eac393fe467900d916a0a8ec8e
Paulo Morosov <paulovictormorosov@hotmail.com>
2023-01-16 19:33:15 -0300

    Commit 2023-01-16

--- index.php
+++ index.php
@@ -6,4 +6,4 @@ require 'vendor/autoload.php';
 $smarty->assign('name', 'Paulo Morosov - 28 Anos - Programador');
 $smarty->assign('routeHome', Routes::get_Home());
 
-$smarty->display('index.tpl');
+$smarty->display('/index.tpl');
